185f75ad4378c0961af573615f79b62dd8d5dc0a
Sashiy1 <aleksander.paluch@wp.pl>
2025-02-07 13:59:31 +0100

    main fix, tsconfig fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import React, { StrictMode } from "react";
 
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-
-const rootElement = document.getElementById('root') as HTMLElement | null;
+const rootElement = document.getElementById("root") as HTMLElement | null;
 
 if (rootElement instanceof HTMLElement) {
-    const root = createRoot(rootElement);
-    root.render(
-    
-            <App />
-       
-    );
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 } else {
-    console.error("Root element not found");
+  console.error("Root element not found");
 }
