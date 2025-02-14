39b43da439422423b45c26fdd1e3c29c89c53400
Sashiy1 <aleksander.paluch@wp.pl>
2025-02-14 13:02:15 +0100

    side bar links types added

--- src/SideBar/SideBarLink.tsx
+++ src/SideBar/SideBarLink.tsx
@@ -1,24 +1,26 @@
 import { motion } from "framer-motion";
+import React from "react";
 
-const SideBarLink = ({ value, href, children, isSelected, setSelected }) => {
+type SideBarLinkValue = "about" | "education" | "projects" | "experience" | "contact";
+
+interface ISideBarLink {
+  value: SideBarLinkValue;
+  href: string;
+  children: React.ReactNode;
+  isSelected: SideBarLinkValue;
+  setSelected: React.Dispatch<React.SetStateAction<SideBarLinkValue>>;
+}
+
+const SideBarLink: React.FC<ISideBarLink> = ({ value, href, children, isSelected, setSelected }) => {
   return (
-    <motion.div
-      initial={{ x: -70 }}
-      animate={{ x: 0 }}
-      transition={{ duration: 0.8, delay: 0.3 }}
-      
-    >
+    <motion.div initial={{ x: -70 }} animate={{ x: 0 }} transition={{ duration: 0.8, delay: 0.3 }}>
       <a
         style={{ rotate: "90deg" }}
-        className={` btn btn-ghost btn-sm w-24 rounded-none text-base min-w-20 md:min-w-[82px]  lg:min-w-[100px] md:btn-md md:w-min lg:text-xl ${
-          isSelected === value
-            ? "border-t-4 border-t-primary bg-neutral text-white"
-            : "border-transparent"
+        className={`btn btn-ghost btn-sm w-24 rounded-none text-base min-w-20 md:min-w-[82px] lg:min-w-[100px] md:btn-md md:w-min lg:text-xl ${
+          isSelected === value ? "border-t-4 border-t-primary bg-neutral text-white" : "border-transparent"
         }`}
         href={href}
-        onClick={() => {
-          setSelected(value);
-        }}
+        onClick={() => setSelected(value)}
       >
         {children}
       </a>
